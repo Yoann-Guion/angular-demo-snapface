9d909c891058cb08414a979fbeef25473dffa15c
Yoann-Guion <yoo.guion@gmail.com>
2024-11-12 14:04:30 +0100

    ajout classe face-snap + ajout de snaps

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,24 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss',
 })
 export class FaceSnapComponent implements OnInit {
-  // (bang)! "promet" à TypeScript que les propriétés seront bien initialisées
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
+
   snapButtonText!: string;
   ifSnap!: boolean;
 
   // Permet d'initialiser les propriétés de la classe
   ngOnInit(): void {
-    this.title = 'Face Snap';
-    this.description =
-      'Face Snap is a simple application that allows you to take a picture of your face and save it to your device.';
-    this.createdAt = new Date();
-    this.snaps = 0;
-    this.imageUrl =
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
     this.snapButtonText = ' Oh Snap !';
     this.ifSnap = false;
   }
@@ -39,14 +29,16 @@ export class FaceSnapComponent implements OnInit {
   }
 
   unSnap() {
-    this.snaps--;
+    this.faceSnap.removeSnap();
+    console.log(this.faceSnap.snaps);
     this.ifSnap = false;
     this.snapButtonText = ' Oh Snap !';
   }
 
   snap() {
-    this.snaps++;
+    this.faceSnap.addSnap();
     this.ifSnap = true;
     this.snapButtonText = 'Delete Snap :(';
+    console.log(this.faceSnap.snaps);
   }
 }

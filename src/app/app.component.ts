db6346acf4ab49bdbd6c5851379a3c194e47a4ad
Yoann-Guion <yoo.guion@gmail.com>
2024-11-12 14:59:56 +0100

    utilisation du @if et @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,31 +10,33 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  mySecondSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      8
-    );
-    this.mySecondSnap = new FaceSnap(
-      'Mr Plop',
-      'Mon pote de la fac',
-      'https://img.freepik.com/vecteurs-libre/plop-pop-art-comic-speech-bubbles-livre-effets-sonores_1142-9509.jpg',
-      new Date(),
-      25
-    );
-    this.myLastSnap = new FaceSnap(
-      'Mme Plop',
-      'La femme de Mr Plop',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg/1200px-%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg',
-      new Date(),
-      3205
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Archibald',
+        'Mon meilleur ami depuis tout petit !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        8
+      ),
+      new FaceSnap(
+        'Mr Plop',
+        'Mon pote de la fac',
+        'https://img.freepik.com/vecteurs-libre/plop-pop-art-comic-speech-bubbles-livre-effets-sonores_1142-9509.jpg',
+        new Date(),
+        25
+      ),
+      new FaceSnap(
+        'Mme Plop',
+        'La femme de Mr Plop',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg/1200px-%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg',
+        new Date(),
+        3205
+      ),
+    ];
+
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }

b6c461efaf80dbeb72ec4a8c3117c778bed9b493
Yoann-Guion <yoo.guion@gmail.com>
2024-11-13 15:32:36 +0100

    ajout d'un service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FaceSnap} from '../models/face-snap';
 import {FaceSnapComponent} from '../face-snap/face-snap.component';
+import {FaceSnapsService} from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -14,29 +15,11 @@ import {FaceSnapComponent} from '../face-snap/face-snap.component';
 export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
+  constructor(private faceSnapsService: FaceSnapsService) {
+
+  }
+
   ngOnInit(): void {
-    this.faceSnaps = [
-      new FaceSnap(
-        'Archibald',
-        'Mon meilleur ami depuis tout petit !',
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        new Date(),
-        8
-      ),
-      new FaceSnap(
-        'Mr Plop',
-        'Mon pote de la fac',
-        'https://img.freepik.com/vecteurs-libre/plop-pop-art-comic-speech-bubbles-livre-effets-sonores_1142-9509.jpg',
-        new Date(),
-        25
-      ),
-      new FaceSnap(
-        'Mme Plop',
-        'La femme de Mr Plop',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg/1200px-%28Venice%29_Peplophoros_in_the_Museo_archeologico_nazionale.jpg',
-        new Date(),
-        3205
-      ),
-    ];
+    this.faceSnaps = this.faceSnapsService.getFaceSnaps();
   }
 }

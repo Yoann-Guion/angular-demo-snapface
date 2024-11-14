80defc6c97672e55de01db4ea679380d947dc729
Yoann-Guion <yoo.guion@gmail.com>
2024-11-14 11:49:46 +0100

    début de routage avec une page d'accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
-import {FaceSnapListComponent} from './face-snap-list/face-snap-list.component';
+import {RouterOutlet} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FaceSnapListComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
